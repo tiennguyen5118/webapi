0d2d90c649df48d7b3a489e539d13c7043fd2a60
Tien Nguyen <tiennguyen5118@gmail.com>
2024-10-03 15:51:30 +0700

    test trigger

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WebApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //test
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
